2822c1edc1161e2462e8a094629ac181f719a223
surya5602 <suryakgm1234@gmail.com>
2022-09-19 09:40:14 +0530

    new

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    script: 'index.js',
+    watch: '.'
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy : {
+    production : {
+      user : 'surya',
+      host : '192.168.1.88',
+      ref  : 'origin/master',
+      repo : 'git@github.com/Surya5602/Basicnodeapp',
+      path : 'opt/deployment/jenkins-nodeserver/source',
+    }
+  }
+};
--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const http = require('http');
 const server =  http.createServer((req, res)=>{
        res.end("Hello from the server!");
 });
-server.listen(8080);
+server.listen(3000);
